071ed6671511c04182ee1289171ecc2c12574a9f
woody2shoes <chuck@lostjungles.com>
2010-04-05 23:00:02 -0600

    we moved complex model forms to jquery.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,15 +3,13 @@
 function insert_fields(link, method, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + method, "g")
-  $(link).up().insert({
-    before: content.replace(regexp, new_id)
-  });
+  $(link).parent().before(content.replace(regexp, new_id));
 }
 
 function remove_fields(link) {
-  var hidden_field = $(link).previous("input[type=hidden]");
+  var hidden_field = $(link).prev("input[type=hidden]");
   if (hidden_field) {
-    hidden_field.value = '1';
+    hidden_field.attr("value", '1');
   }
-  $(link).up(".fields").hide();
+  $(link).parent(".fields").hide();
 }
